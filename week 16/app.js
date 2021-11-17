1a4306c556ecb999d4854afd2c65041310b477c7
stasik132 <72654421+stasik132@users.noreply.github.com>
2021-11-17 18:23:22 +0200

    Add files via upload

---
+++ week 16/app.js
@@ -0,0 +1,12 @@
+var car = {
+    speed :"60km/h",
+    name:"Lamba",
+    color:"red",
+    toString:function(){
+   return `name: ${this.name}; speed: ${this.speed}; color: ${this.red}`
+  }
+  };
+  console.log(car.speed);
+  console.log(car.name);
+  console.log(car.color);
+  console.log(car.toString());
